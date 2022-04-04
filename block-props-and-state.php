19d79e73ac00db37c6d9b381f68a06e020ee5780
Seth Miller <seth@flexperception.com>
2022-04-03 23:57:53 -0400

    Initial commit of block

---
+++ block-props-and-state.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name:       Block Props And State
+ * Description:       Example block written with ESNext standard and JSX support – build step required.
+ * Requires at least: 5.8
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            The WordPress Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       block-props-and-state
+ *
+ * @package           create-block
+ */
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function create_block_block_props_and_state_block_init() {
+	register_block_type( __DIR__ . '/build' );
+}
+add_action( 'init', 'create_block_block_props_and_state_block_init' );
---
+++ src/edit.js
@@ -0,0 +1,72 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
+ */
+import { useBlockProps } from '@wordpress/block-editor';
+
+import { useState } from '@wordpress/element';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
+ *
+ * @return {WPElement} Element to render.
+ */
+ const Edit = ( props ) => {
+
+	const {
+		attributes: { 
+			isLoaded,
+			inputText
+		}
+	} = props;
+
+	// for persisting block data 
+	const [ blockData, setBlockData ] = useState( props.attributes );
+
+	return (
+		<p {...useBlockProps()}>
+
+		<input type="text" value={props.attributes.inputText}
+			// onChange={ (e) => { setBlockData(inputText: e.target.value) } }
+			onChange={ 
+				(e) => { 
+					setBlockData({ inputText: e.target.value }) 
+					// realtime changes in the editor
+					props.setAttributes({ inputText: e.target.value })	
+					// saves block data for later use
+				} }
+		/>
+
+		<br />
+
+			{__(
+				'Block Props And State – hello from the editor!',
+				'block-props-and-state'
+			)}
+			<br />Input text: { blockData.inputText }
+		</p>
+	);
+}
+
+export default Edit;
+
